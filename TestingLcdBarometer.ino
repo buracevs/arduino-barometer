ecfdfb8c7ed62bb45a9d152304e198726576d095
buracevs <buracevs@gmail.com>
2019-12-04 09:17:33 +0200

    added ethernet shield support for post requests

--- TestingLcdBarometer.ino
+++ TestingLcdBarometer.ino
@@ -1,54 +1,83 @@
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Adafruit_BMP085.h>
+#include <Ethernet.h>
 
 
-LiquidCrystal_I2C lcd(0x3F, 2,1,0,4,5,6,7,3, POSITIVE);
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 Adafruit_BMP085 sensor = Adafruit_BMP085();
- 
+
+byte mac [] = {0x90, 0xA2, 0xDA, 0x00, 0x4A, 0xE0};
+char serverName[] = "192.168.0.14";
+int serverPort = 80;
+EthernetClient client;
+
+
 void setup() {
-  
-Serial.begin(9600);
-lcd.begin(16,2);
-//pinMode(ledPort, OUTPUT);
-  if(!sensor.begin()){
-    Serial.println("Sensor cannot be found");
-    lcd.setCursor(0, 0);  
-    lcd.print("Sensor cannot be found");
-    while(1){}
+
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+
+  if (!Ethernet.begin(mac)) {
+    Serial.println("Error, could not configure with DHCP");
+  }
+  else {
+    Serial.println("Ethernet configured succsessfully.");
+    Serial.println("Arduino IP:" + Ethernet.localIP());
   }
 
-
+  if (!sensor.begin()) {
+    Serial.println("Sensor cannot be found");
+    lcd.setCursor(0, 0);
+    lcd.print("Sensor cannot be found");
+    while (1) {}
+  }
 }
 
 void loop() {
-/*Serial.println("Data from Arduino Sensor "); */
- /*lcd.setCursor(0, 0);
- lcd.print("Data from Arduino Sensor ");
- delay(3000);*/
-
-  Serial.print("Temperature = ");
-  Serial.print(sensor.readTemperature());
-  Serial.println("*C");
-
-  lcd.setCursor(0, 0); 
+  float temperature = sensor.readTemperature();
+  lcd.setCursor(0, 0);
   lcd.print("Temp.= ");
-  lcd.print(sensor.readTemperature());
+  lcd.print(temperature);
   lcd.print(" C");
 
-  float pressure = sensor.readPressure()/101.325;
-  pressure*=0.760;
+  float pressure = sensor.readPressure() / 101.325;
+  pressure *= 0.760;
   lcd.setCursor(0, 1);
-  lcd.print("Pres= "); 
+  lcd.print("Pres= ");
   lcd.print(pressure);
   lcd.print("mmHg");
 
-  Serial.print("Pressure = ");
-  Serial.print(pressure);
-  Serial.println(" mmHg");
-  
+  connectToServer(temperature, pressure);
+
   delay(2000);
+}
 
+void connectToServer(float temperature, float pressure) {
+  String data = "{";
+  data = data + "'temperature':" + temperature + ",";
+  data = data + "'pressure':" + pressure + "}";
+  Serial.println("data to send:" + data);
+  if (client.connect(serverName, serverPort)) {
+    Serial.println("Connected to server");
 
-  
+    client.print("POST /test ");
+    client.println("HTTP/1.1");
+    client.print("Host: ");
+    client.println(serverName);
+    client.println("User-Agent: Arduino/1.0");
+    client.println("Connection: close");
+    client.println("Content-Type: application/javascript;");
+    client.print("Content-Length: ");
+    client.println(data.length());
+    client.println();
+    client.println(data);
+    
+    Serial.println("complete");
+
+  } else {
+    Serial.println("connection failed");
+  }
+
+  client.stop();
 }
